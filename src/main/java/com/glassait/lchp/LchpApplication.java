3503e64adda1742de4588a378a9cb8d578991174
Glassait <tendzinroffler@gmail.com>
2023-08-06 15:00:58 +0200

    Initial commit

---
+++ src/main/java/com/glassait/lchp/LchpApplication.java
@@ -0,0 +1,13 @@
+package com.glassait.lchp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LchpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LchpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glassait/lchp/LchpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glassait.lchp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LchpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
