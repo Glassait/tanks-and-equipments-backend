d8bcbc388caad034ce86a9d44da8d241210e8825
Glassait <tendzinroffler@gmail.com>
2023-10-22 13:14:23 +0200

    feat(1.0): Update Member api

--- src/main/java/com/glassait/lchp/controllers/MemberController.java
+++ src/main/java/com/glassait/lchp/controllers/MemberController.java
@@ -1,26 +1,37 @@
 package com.glassait.lchp.controllers;
 
+import com.glassait.lchp.abstracts.GlassaitLogger;
 import com.glassait.lchp.abstracts.membre.Member;
 import com.glassait.lchp.services.MembreService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.glassait.lchp.services.WotService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
 @RestController
-public class MemberController {
-    @Autowired
-    private MembreService membreService;
+@RequiredArgsConstructor
+public class MemberController extends GlassaitLogger {
+    private final MembreService membreService;
+    private final WotService wotService;
 
-    @RequestMapping(value = "api/member/{account_id}")
-    public Member isClanMember(@PathVariable("account_id") String accountId) {
-        return membreService.isClanMember(Integer.parseInt(accountId)).map(Member::new).orElse(null);
+    @RequestMapping(value = "api/member")
+    public ResponseEntity<Member> isClanMember(@RequestParam("account_id") Integer accountId) {
+        Member member = membreService.findById(accountId).map(Member::new).orElse(null);
+        return new ResponseEntity<>(member, HttpStatus.OK);
     }
 
     @RequestMapping(value = "api/member/update")
-    public boolean updateClanMember() {
-        return membreService.updateMembers();
+    public ResponseEntity<String> updateClanMember(@RequestParam(name = "access_token") String accessToken) {
+        if (this.wotService.checkAccessToken(accessToken)) {
+            super.logDebug("The access token {" + accessToken + "} is valide");
+            return new ResponseEntity<>(this.membreService.updateMembers(), HttpStatus.OK);
+        }
+        super.logError("The access token {" + accessToken + "} is not valide or the user is not a member of the clan");
+        return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
     }
 }
