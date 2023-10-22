ef181de0fc93d641b50d9e26c9033f21b6e24537
Glassait <tendzinroffler@gmail.com>
2023-10-22 12:49:59 +0200

    feat(1.0): Update Tank api

--- src/main/java/com/glassait/lchp/controllers/TankController.java
+++ src/main/java/com/glassait/lchp/controllers/TankController.java
@@ -1,28 +1,33 @@
 package com.glassait.lchp.controllers;
 
+import com.glassait.lchp.abstracts.GlassaitLogger;
 import com.glassait.lchp.abstracts.tankData.Tank;
 import com.glassait.lchp.services.TankService;
 import com.glassait.lchp.services.WotService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @CrossOrigin("*")
-@RequestMapping(value = "api/tanks/{access_token}")
 @RestController
-public class TankController {
-    @Autowired
-    private TankService tankService;
-    @Autowired
-    private WotService wotService;
+@RequiredArgsConstructor
+public class TankController extends GlassaitLogger {
+    private final TankService tankService;
+    private final WotService wotService;
 
-    @GetMapping
-    public List<Tank> getStudents(@PathVariable("access_token") String accessToken) {
-        if (wotService.checkAccessToken(accessToken)) {
-            return tankService.getTanks();
+    @GetMapping(value = "api/tanks")
+    public ResponseEntity<List<Tank>> getStudents(@RequestParam(name = "access_token") String accessToken) {
+        if (this.wotService.checkAccessToken(accessToken)) {
+            super.logDebug("The access token {" + accessToken + "} is valide");
+            return new ResponseEntity<>(this.tankService.getTanks(), HttpStatus.OK);
         }
-        System.out.println("Token : `" + accessToken + "` is invalid or has expire");
-        return null;
+        super.logError("The access token {" + accessToken + "} is not valide or the user is not a member of the clan");
+        return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
     }
 }
--- src/main/java/com/glassait/lchp/services/TankService.java
+++ src/main/java/com/glassait/lchp/services/TankService.java
@@ -3,24 +3,27 @@ package com.glassait.lchp.services;
 import com.glassait.lchp.abstracts.tankData.Tank;
 import com.glassait.lchp.model.tank.TankModel;
 import com.glassait.lchp.repositories.TankRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TankService {
-    @Autowired
-    private TankRepository tankRepository;
+    private final TankRepository tankRepository;
 
+    /**
+     * Get all the tanks data from the database
+     *
+     * @return The list of tanks data
+     */
     public List<Tank> getTanks() {
         List<TankModel> tankModels = tankRepository.findAll();
         List<Tank> tanks = new ArrayList<>();
 
-        tankModels.forEach(tankModel -> {
-            tanks.add(new Tank(tankModel));
-        });
+        tankModels.forEach(tankModel -> tanks.add(new Tank(tankModel)));
 
         return tanks;
     }
