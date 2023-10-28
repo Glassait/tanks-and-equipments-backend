8213dc05333745b39200df8189af66b7477d30c9
Glassait <tendzinroffler@gmail.com>
2023-10-28 11:01:47 +0200

    feat(1.0): Update Member api 2

--- src/main/java/com/glassait/lchp/controllers/MemberController.java
+++ src/main/java/com/glassait/lchp/controllers/MemberController.java
@@ -7,10 +7,7 @@ import com.glassait.lchp.services.WotService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
 @RestController
@@ -19,13 +16,13 @@ public class MemberController extends GlassaitLogger {
     private final MembreService membreService;
     private final WotService wotService;
 
-    @RequestMapping(value = "api/member")
+    @GetMapping(value = "api/member")
     public ResponseEntity<Member> isClanMember(@RequestParam("account_id") Integer accountId) {
         Member member = membreService.findById(accountId).map(Member::new).orElse(null);
         return new ResponseEntity<>(member, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "api/member/update")
+    @PostMapping(value = "api/member/update")
     public ResponseEntity<String> updateClanMember(@RequestParam(name = "access_token") String accessToken) {
         if (this.wotService.checkAccessToken(accessToken)) {
             return new ResponseEntity<>(this.membreService.updateMembers(), HttpStatus.OK);
