3b4faf60364803411602409b8af5c5f54fc8736f
glassait <tendzinroffler@gmail.com>
2024-01-25 20:20:53 +0100

    change backend architecture

--- backend/src/test/java/com/glassait/equipment_tanks/GlassaitApplicationTests.java
+++ backend/src/test/java/com/glassait/equipment_tanks/GlassaitApplicationTests.java
@@ -1,9 +1,6 @@
 package com.glassait.equipment_tanks;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class GlassaitApplicationTests {}
