0b2bb56bccb935d71918cc63c11f5bf4ba1f7afe
glassait <tendzinroffler@gmail.com>
2024-10-14 23:48:46 +0200

    Rework config and add security

--- backend/src/main/java/com/glassait/tanks_and_equipments/GlassaitApplication.java
+++ backend/src/main/java/com/glassait/tanks_and_equipments/GlassaitApplication.java
@@ -3,20 +3,13 @@ package com.glassait.tanks_and_equipments;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-/**
- * Main class of the Glassait application.
- *
- * @author Glassait
- */
-@SpringBootApplication
 @Slf4j
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableWebSecurity
 public class GlassaitApplication {
-    /**
-     * Main method of the application.
-     *
-     * @param args command line arguments
-     */
     public static void main(String[] args) {
         SpringApplication.run(GlassaitApplication.class, args);
         log.info("YAHOO, The application is running properly");
---
+++ backend/src/main/java/com/glassait/tanks_and_equipments/config/UnsecuredConfig.java
@@ -0,0 +1,20 @@
+package com.glassait.tanks_and_equipments.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("unsecured")
+public class UnsecuredConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(request -> request.anyRequest().permitAll())
+                   .httpBasic(Customizer.withDefaults())
+                   .build();
+    }
+}
