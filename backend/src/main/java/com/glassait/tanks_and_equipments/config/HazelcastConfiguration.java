54c2252ec25391ef7203f9bc52a89fb5a894a373
glassait <tendzinroffler@gmail.com>
2024-10-15 20:21:01 +0200

    Add cache manager

--- backend/src/main/java/com/glassait/tanks_and_equipments/config/HazelcastConfiguration.java
+++ backend/src/main/java/com/glassait/tanks_and_equipments/config/HazelcastConfiguration.java
@@ -4,8 +4,13 @@ import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Set;
 
 @Configuration
 public class HazelcastConfiguration {
@@ -17,5 +22,12 @@ public class HazelcastConfiguration {
         Config config = new Config();
         config.setInstanceName(hazelcastInstanceName);
         return Hazelcast.newHazelcastInstance(config);
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(Set.of(new ConcurrentMapCache("default")));
+        return cacheManager;
     }
 }
